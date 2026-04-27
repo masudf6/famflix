8325c451fb8ae0e7deb7d8161496ccc1fb5a5adc
Masud <masud.f6@gmail.com>
2026-04-28 02:59:46 +1000

    add image output from ci to cd

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
-from app.db import Base, engine
+import socket
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.db import Base, engine
 from app.models.user import User
 from app.models.media import Media
 from app.models.media_file import MediaFile
-
 from app.routes.auth import router as auth_router
 from app.routes.media import router as media_router
 
@@ -12,32 +14,32 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Family Streaming Platform API")
 
-app.include_router(auth_router)
-app.include_router(media_router)
-
-from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://famflix.local",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.include_router(auth_router)
+app.include_router(media_router)
+
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-import socket
 
 @app.get("/whoami")
 def whoami():
-    return {"I am pod": socket.gethostname()}
+    return {"pod": socket.gethostname()}
